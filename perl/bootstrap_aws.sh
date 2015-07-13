429a7bf032bb80b13451fbe820bb5b1c0acd1bac
Michelangelo D'Agostino <mdagost@gmail.com>
2015-07-12 19:42:08 -0500

    Updated Dockerfile to include perl SES module.  Added timezone and NTP to AWS bootstrap.

--- perl/bootstrap_aws.sh
+++ perl/bootstrap_aws.sh
@@ -1,6 +1,8 @@
 # ami-d05e75b8, t2.medium
 sudo apt-get update
-sudo apt-get install -y emacs git tmux
+sudo apt-get install -y emacs git tmux ntp
+
+sudo timedatectl set-timezone America/Chicago
 
 wget -qO- https://get.docker.com/ | sh
 
@@ -9,9 +11,11 @@ sudo usermod -aG docker mdagost
 
 sudo su mdagost
 docker login
+
 cd /home/mdagost/
 git clone https://github.com/mdagost/autoswa.git
 
+docker pull mdagost/perl_mechanize
 docker run -it -v /home/mdagost/autoswa:/app mdagost/perl_mechanize perl /app/perl/scripts/doCheckin.pl CONF FN LN email
 
 # example crontab entry
